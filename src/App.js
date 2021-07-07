8338425655096beb9c1cfc1d8a8a3665f9f1a1e5
Laura Pociuniene <laura.pociuniene@gmail.com>
2021-07-07 13:25:04 +0300

    created input validation, focus, and unit tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [validationMessage, setValidationMessage] = useState("");
+  const inputRef = useRef();
+
+  const hanleInputChange = (event) => {
+    event.preventDefault();
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("woof");
+    if (inputValue !== inputValue.toLowerCase()) {
+      inputRef.current.focus();
+      setValidationMessage("");
+    } else {
+      setValidationMessage("First name can not be all lowercase");
+    }
+  };
+
+  window.addEventListener("beforeunload", function (e) {
+    e.preventDefault();
+    e.returnValue = "";
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ margin: "10px 0 0 0" }}>
+        First name:
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={inputValue}
+          ref={inputRef}
+          onChange={hanleInputChange}
+        />
+        <p style={{ color: "red" }}>{validationMessage}</p>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App", () => {
+  beforeEach(() => {
+    let app = render(<App />);
+  });
+
+  it("should have textbox element", () => {
+    const textInputElement = screen.getByRole("textbox");
+    // console.log(textInputElement);
+    expect(textInputElement).toBeDefined();
+  });
+
+  describe("handleSubmit", () => {
+    it("sets validation message on button click", () => {
+      const submitButton = screen.getByRole("button");
+      // console.log(submitButton);
+      userEvent.click(submitButton);
+      expect(
+        screen.getByText("First name can not be all lowercase")
+      ).toBeInTheDocument();
+    });
+    it("shows no validation message on button click with proper input", () => {
+      const submitButton = screen.getByRole("button");
+      const textInputbox = screen.getByRole("textbox");
+      // console.log(textInputbox);
+      userEvent.type(textInputbox, "blahA");
+      userEvent.click(submitButton);
+      const errorMessage = screen.queryByText(
+        "First name can not be all lowercase",
+        {}
+      );
+      expect(errorMessage).not.toBeInTheDocument();
+    });
+  });
 });
